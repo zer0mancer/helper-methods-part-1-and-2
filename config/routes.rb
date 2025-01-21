4d17d7448588d85c61afef51d80321407eef03d6
Jim Vongsouvanh <95146291+zer0mancer@users.noreply.github.com>
2025-01-21 09:28:14 +0000

    refactored routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,43 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "movies", :action => "index" })
+
+
+  root "movies#index"
+  get "/",  controller: "movies", action: "index" 
+
+  get "/" => "movies#index"
 
   # Routes for the Movie resource:
 
   # CREATE
-  post("/movies", { :controller => "movies", :action => "create" })
-  get("/movies/new", { :controller => "movies", :action => "new" })
+  post"/movies",  controller: "movies", action: "create"
+  get"/movies/new",  controller: "movies", action: "new" 
+
+  post "/movies" => "movies#create"
+  get "/movies/new" => "movies#new"
+  
+
           
   # READ
-  get("/movies", { :controller => "movies", :action => "index" })
-  get("/movies/:id", { :controller => "movies", :action => "show" })
+
+  get "/movies" => "movies#index"
+  get "/movies/:id" => "movies#show"
+
+
+  get"/movies", controller: "movies", action: "index" 
+  get "/movies/:id", controller: "movies", action: "show"
   
   # UPDATE
-  patch("/movies/:id", { :controller => "movies", :action => "update" })
-  get("/movies/:id/edit", { :controller => "movies", :action => "edit" })
+
+  patch "/movies/:id" => "movies#update"
+  get "/movies/:id/edit" => "movies#edit"
+
+  patch"/movies/:id", controller: "movies", action: "update" 
+  get"/movies/:id/edit", controller: "movies", action: "edit" 
   
   # DELETE
-  delete("/movies/:id", { :controller => "movies", :action => "destroy" })
 
-  #------------------------------
+  delete "/movies/:id" => "movies#destroy"
+
+  delete "/movies/:id", controller: "movies", action: "destroy"
+
 end
