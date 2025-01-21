71e7f94909d94c71d8f71a44affa8bd88e306013
Jim Vongsouvanh <95146291+zer0mancer@users.noreply.github.com>
2025-01-21 10:01:48 +0000

    shortcut used instead of direct paths

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -38,7 +38,7 @@ class MoviesController < ApplicationController
 
     if @the_movie.valid?
       @the_movie.save
-      redirect_to("/movies", { :notice => "Movie created successfully." })
+      redirect_to(movies_url, { :notice => "Movie created successfully." })
     else
       render template: "movies/new"
     end
@@ -63,9 +63,9 @@ class MoviesController < ApplicationController
 
     if the_movie.valid?
       the_movie.save
-      redirect_to("/movies/#{the_movie.id}", { :notice => "Movie updated successfully."} )
+      redirect_to(movie_url(the_movie), { :notice => "Movie updated successfully."} )
     else
-      redirect_to("/movies/#{the_movie.id}", { :alert => "Movie failed to update successfully." })
+      redirect_to(movie_url(the_movie), { :alert => "Movie failed to update successfully." })
     end
   end
 
@@ -75,6 +75,6 @@ class MoviesController < ApplicationController
 
     the_movie.destroy
 
-    redirect_to("/movies", { :notice => "Movie deleted successfully."} )
+    redirect_to(movies_url, { :notice => "Movie deleted successfully."} )
   end
 end
